7104350ff5aef1f3dc4b509bd1a1f7d2f7c6cf46
Adarsh Pai <adarsh9pai@gmail.com>
2020-09-19 22:42:25 -0500

    Add First Content Skew

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,57 @@
 import React from 'react';
 import './App.css';
-import MainContent from './MainContent';
-import { Layout, Menu, Typography } from 'antd';
-
+import Icon from '@ant-design/icons';
+import Logo from './logo.png';
+import { Layout, Menu, Typography, Row, Col, Avatar } from 'antd';
 const { Header, Content, Footer } = Layout;
 const { Text } = Typography;
 
 function App() {
+
+  const NavBar = () => {
+    return (
+      <Row align='middle'>
+        <Col span={4}>
+          <Row align='middle'>
+          <Avatar size={40} src={Logo} style={{border: '1px solid'}}/>
+          </Row>
+        </Col>
+        <Col span={16}>
+          <Row justify='center'>
+            <Menu style={{background:'transparent', color:'black', borderBottom:'0'}} mode="horizontal">
+            </Menu>
+          </Row>
+        </Col>
+        <Col span={4}>
+          <Row justify='center'>
+            <Text style={{color: 'white'}}>Launching <Text strong style={{color: 'white'}}>Q1 2021</Text></Text>
+          </Row>
+        </Col>
+      </Row>
+    )}
+
   return (
-    <Layout>
-      <Content className='homepage_first_content'>
-        <Menu className='homepage_first_layout_menu' mode="horizontal">
-          <Text style={{position: 'absolute', right: '5vw'}}>Launching <Text strong>Q1 2021</Text></Text>
-        </Menu>
-        <div className='homepage_skew'/>
+    <Layout style={{minHeight: '100vh'}}>
+      <Layout className='homepage_first_layout'>
+        <Content className='homepage_gradient_skew'>
+          <Header className='homepage_header'>
+            <NavBar />
+          </Header>
+          <Content style={{marginTop: '15vh'}}>
+            <Row>
+              <Col span={4}/>
+              <Col span={8}>
+                <Typography.Title>Abcdefg Hijklmnop</Typography.Title>
+                <p>
+                  Leverage the power of Git through a minimal and intuitive interface.
+                </p>
+              </Col>
+              <Col span={16}/>
+            </Row>
+          </Content>
+        </Content>
+      </Layout>
+      <Content>
       </Content>
       <Footer className='homepage_footer'>© Gitraffe</Footer>
     </Layout>
