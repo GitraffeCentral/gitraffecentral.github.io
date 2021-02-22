4ab7b10ce163da4eee816f79250c98c9775f735a
James Brady <jbrady9559@gmail.com>
2021-02-22 02:01:54 -0600

    Updates

---
+++ precache-manifest.75905a219caa00ee1ca515448bdb041d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "587855a70a8aa50fa54149c35f53f838",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b345ca59d04b8ed4914",
+    "url": "/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "fef094f94c350b3630c9",
+    "url": "/static/css/main.c7e58f55.chunk.css"
+  },
+  {
+    "revision": "7b345ca59d04b8ed4914",
+    "url": "/static/js/2.bb531bef.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.bb531bef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fef094f94c350b3630c9",
+    "url": "/static/js/main.f9e5be54.chunk.js"
+  },
+  {
+    "revision": "eca5adf42beec5911a9f",
+    "url": "/static/js/runtime-main.e5bb7506.js"
+  },
+  {
+    "revision": "ee0e8b6eb32bebdf11b9f9a6387e6e2d",
+    "url": "/static/media/logo.ee0e8b6e.png"
+  }
+]);
