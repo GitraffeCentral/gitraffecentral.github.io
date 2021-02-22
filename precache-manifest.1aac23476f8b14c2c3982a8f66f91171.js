7518fcc562f505782dac9c091be9ee2efb1cc1f0
James Brady <jbrady9559@gmail.com>
2021-02-22 02:19:45 -0600

    Updates

---
+++ precache-manifest.1aac23476f8b14c2c3982a8f66f91171.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b947b9be7caf96ac61317dda8e902517",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5804a3187211d1344db1",
+    "url": "./static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "9d0d4fb91b8bcd108fa1",
+    "url": "./static/css/main.c7e58f55.chunk.css"
+  },
+  {
+    "revision": "5804a3187211d1344db1",
+    "url": "./static/js/2.6f5a2265.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.6f5a2265.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d0d4fb91b8bcd108fa1",
+    "url": "./static/js/main.4728e797.chunk.js"
+  },
+  {
+    "revision": "4ebc06d70f48cd25f397",
+    "url": "./static/js/runtime-main.218341dc.js"
+  },
+  {
+    "revision": "ee0e8b6eb32bebdf11b9f9a6387e6e2d",
+    "url": "./static/media/logo.ee0e8b6e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.75905a219caa00ee1ca515448bdb041d.js"
+  "./precache-manifest.1aac23476f8b14c2c3982a8f66f91171.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
