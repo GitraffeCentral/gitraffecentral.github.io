b39f4ef7e3d451b2c9618f8acd7d8caa47d40547
Adarsh Yogesh Pai <adarsh9pai@gmail.com>
2021-02-22 05:11:29 -0600

    Add styles in accordance with Gitraffe theme

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,17 @@
-import { PageHeader } from "antd";
+import { PageHeader, Button, Row } from "antd";
+import { ArrowRightOutlined } from '@ant-design/icons';
 import React from "react";
 import "./index.css";
 
 const Header = () => {
-    return <PageHeader className="header_root" title="Gitraffe" ghost={false} extra={<div className="header_release-date"> Private beta launching soon</div>} />;
+    return (
+    <PageHeader className="header_root" title="" ghost={false} extra={
+        <a href="https://x836on4k62z.typeform.com/to/KGopr4aR">
+            <Button type="primary" shape="round" size={"large"}  style={{background: "linear-gradient(45deg, #ee0979, #ff6a00)", borderWidth: '0', verticalAlign: "middle"}}>
+                    Request Access <ArrowRightOutlined />
+            </Button>
+    </a>} />
+    );
 };
 
 export default Header;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { Layout, Typography, Row } from "antd";
 import "./index.css";
 
+const {Content}  = Layout;
+const { Title, Paragraph } = Typography;
+
 const Home = () => {
-  return <div> home page </div>;
+  return (
+    <Content style={{background: "#1f1f1f", width: "100vw", minHeight: "90vh"}}>
+      <Row align="middle" justify="center" style={{width: "100%"}}>
+        <Title style={{fontSize: "8vh", color: "#a0aec0"}}>Aspirin for Version Control.</Title>
+      </Row>
+      <Row align="middle" justify="center" style={{width: "100%"}}>
+        <div style={{color: "white", fontSize: "2vh", textAlign: "center"}}><strong>Gitraffe</strong> helps you leverage the power of Git through a minimal and intuitive interface.
+        <br/>Minimize hiccups with Interactive Rebase, Cherry Picking, Reflog, Commit + History Editing, Quick Actions, and Tailored Workspaces.</div>
+      </Row>
+    </Content>
+  );
 };
 
 export default Home;
