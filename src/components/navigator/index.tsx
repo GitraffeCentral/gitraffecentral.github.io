9423f062ac643ffbffcc3aa431b8144a78d48566
James Brady <jbrady9559@gmail.com>
2021-03-03 19:40:03 -0600

    Use hash router instead of browser rotuer

--- src/components/navigator/index.tsx
+++ src/components/navigator/index.tsx
@@ -1,26 +1,25 @@
 import React from "react";
-import { BrowserRouter, Route, Switch as RouterSwitch } from "react-router-dom";
+import { HashRouter, Route, Switch as RouterSwitch } from "react-router-dom";
 import Home from "../../pages/home";
-import Bitbucket from "../../pages/bitbucket"
-import Gitlab from "../../pages/gitlab"
+import Bitbucket from "../../pages/bitbucket";
+import Gitlab from "../../pages/gitlab";
 
 const Navigator = () => {
-
-    return (
-        <BrowserRouter>
-            <RouterSwitch>
-                <Route exact path="/">
-                    <Home />
-                </Route>
-                <Route exact path="/oauth2/bitbucket/callback">
-                    <Bitbucket />
-                </Route>
-                <Route exact path="/oauth2/gitlab/callback">
-                    <Gitlab />
-                </Route>
-            </RouterSwitch>
-        </BrowserRouter>
-    )
-}
+  return (
+    <HashRouter basename={process.env.PUBLIC_URL}>
+      <RouterSwitch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/oauth2/bitbucket/callback">
+          <Bitbucket />
+        </Route>
+        <Route exact path="/oauth2/gitlab/callback">
+          <Gitlab />
+        </Route>
+      </RouterSwitch>
+    </HashRouter>
+  );
+};
 
 export default Navigator;
