8bac0846e8ce451f312534e44b71a204a054643c
Adarsh Yogesh Pai <adarsh9pai@gmail.com>
2021-03-03 20:12:53 -0600

    Change routing strategy and switch back to BrowserRouter

--- src/components/navigator/index.tsx
+++ src/components/navigator/index.tsx
@@ -1,24 +1,18 @@
 import React from "react";
-import { HashRouter, Route, Switch as RouterSwitch } from "react-router-dom";
+import { BrowserRouter, Route, Switch as RouterSwitch } from "react-router-dom";
 import Home from "../../pages/home";
 import Bitbucket from "../../pages/bitbucket";
 import Gitlab from "../../pages/gitlab";
 
 const Navigator = () => {
   return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <RouterSwitch>
         <Route exact path="/">
           <Home />
         </Route>
-        <Route exact path="/oauth2/bitbucket/callback">
-          <Bitbucket />
-        </Route>
-        <Route exact path="/oauth2/gitlab/callback">
-          <Gitlab />
-        </Route>
       </RouterSwitch>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,73 +5,86 @@ import { FaUbuntu } from "react-icons/fa";
 import "./index.css";
 import InteractiveRebaseMac from "../../resources/interactive-rebase-mac.png";
 import Header from "../../components/header";
+import { useLocation, withRouter } from "react-router";
+import Bitbucket, {} from "../bitbucket";
+import Gitlab, {} from "../gitlab";
 
 const { Content, Footer } = Layout;
 const { Title, Paragraph, Text } = Typography;
 
 const Home = () => {
-  return (
-    <div>
-       <Header />
-    <div
-      style={{
-        background: "#1f1f1f",
-        width: "100%",
-        minHeight: "100vh",
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <Row
-        align="middle"
-        justify="center"
-        style={{ width: "100%", flex: "0 0 auto" }}
-      >
-        <Title
-          style={{ fontSize: "8vh", color: "#a0aec0", textAlign: "center" }}
-        >
-          Aspirin for Version Control.
-        </Title>
-      </Row>
-      <Row
-        align="middle"
-        justify="center"
-        style={{ width: "100%", flex: "0 0 auto" }}
-      >
-        <div style={{ color: "white", fontSize: "2vh", textAlign: "center" }}>
-          <strong>Gitraffe</strong> helps you leverage the power of Git through
-          a beautiful and intuitive interface.
-          <br />
-          Minimize Git hiccups with Interactive Rebase, Cherry Picking, Reflog,
-          Commit + History Editing, Quick Actions, and Tailored Workspaces.
-          <br />
-          <br/>
-          Available on Windows, Mac, and Linux.
-        </div>
-      </Row>
-      <Row
-        align="middle"
-        justify="center"
-        style={{ width: "100", flex: "1 1", marginTop: "5vh", marginBottom: "5vh" }}
-      >
-        <img src={InteractiveRebaseMac} width="60%" style={{filter: "drop-shadow(0 0 1rem #2a2a2a)"}}/>
-      </Row>
+  const location = useLocation();
+  let authCode = new URLSearchParams(location.search).get("code");
+  let provider = new URLSearchParams(location.search).get("provider");
 
-      <Footer style={{
-        background: "#1f1f1f",
-        width: "100%",
-      }}>
-              <Row
-        align="middle"
-        justify="center"
-        style={{ width: "100%", flex: "0 0 auto" }}
+  console.log(location);
+
+  if(provider == "bitbucket") return <Bitbucket />
+  else if(provider == "gitlab") return <Gitlab/>
+  else {
+    return (
+      <div>
+         <Header />
+      <div
+        style={{
+          background: "#1f1f1f",
+          width: "100%",
+          minHeight: "100vh",
+          display: "flex",
+          flexDirection: "column",
+        }}
       >
-        <Title level={5} style={{color: "white", fontWeight: "normal"}}>Team Raffe, 2021</Title>
-      </Row>
-      </Footer>
-    </div>
-    </div>
-  );
+        <Row
+          align="middle"
+          justify="center"
+          style={{ width: "100%", flex: "0 0 auto" }}
+        >
+          <Title
+            style={{ fontSize: "8vh", color: "#a0aec0", textAlign: "center" }}
+          >
+            Aspirin for Version Control.
+          </Title>
+        </Row>
+        <Row
+          align="middle"
+          justify="center"
+          style={{ width: "100%", flex: "0 0 auto" }}
+        >
+          <div style={{ color: "white", fontSize: "2vh", textAlign: "center" }}>
+            <strong>Gitraffe</strong> helps you leverage the power of Git through
+            a beautiful and intuitive interface.
+            <br />
+            Minimize Git hiccups with Interactive Rebase, Cherry Picking, Reflog,
+            Commit + History Editing, Quick Actions, and Tailored Workspaces.
+            <br />
+            <br/>
+            Available on Windows, Mac, and Linux.
+          </div>
+        </Row>
+        <Row
+          align="middle"
+          justify="center"
+          style={{ width: "100", flex: "1 1", marginTop: "5vh", marginBottom: "5vh" }}
+        >
+          <img src={InteractiveRebaseMac} width="60%" style={{filter: "drop-shadow(0 0 1rem #2a2a2a)"}}/>
+        </Row>
+  
+        <Footer style={{
+          background: "#1f1f1f",
+          width: "100%",
+        }}>
+                <Row
+          align="middle"
+          justify="center"
+          style={{ width: "100%", flex: "0 0 auto" }}
+        >
+          <Title level={5} style={{color: "white", fontWeight: "normal"}}>Team Raffe, 2021</Title>
+        </Row>
+        </Footer>
+      </div>
+      </div>
+    );
+  }
 };
 
-export default Home;
+export default withRouter(Home);
